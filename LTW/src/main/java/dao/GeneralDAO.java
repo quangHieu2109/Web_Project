644956eb5092eab70b869a0b9d29b2e5762842cb
hai000 <21130343@st.hcmuaf.edu.vn>
2023-12-03 10:37:29 +0700

    update

---
+++ LTW/src/main/java/dao/GeneralDAO.java
@@ -0,0 +1,9 @@
+package dao;
+
+import java.sql.Connection;
+
+import database.JDBCUtil;
+
+public class GeneralDAO {
+protected JDBCUtil dbcon = new JDBCUtil();
+}
---
+++ LTW/src/main/java/dao/TheLoaiDAO.java
@@ -0,0 +1,25 @@
+package dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import model.BaiBao;
+import model.TheLoai;
+
+public class TheLoaiDAO extends GeneralDAO {
+public List<TheLoai> getDSTheLoai() {
+	List<TheLoai> result = new ArrayList<TheLoai>();
+//	ArrayList<String> values = new ArrayList<String>();
+//	values.add("kinh te");
+//	values.add("doi");
+//	result.add(new TheLoai("chinh tri", values)); Test data
+	// 
+	return result;
+}
+public void addTheLoai(BaiBao baiBao) {
+	
+}
+public void removeTheLoai(BaiBao baiBao) {
+	
+}
+}
