ec808572f1a083ec3f1a0f5d8706700651000aa6
hai000 <21130343@st.hcmuaf.edu.vn>
2023-12-12 18:58:46 +0700

    update upload news

--- LTW/src/main/java/controller/MainServlet.java
+++ LTW/src/main/java/controller/MainServlet.java
@@ -7,11 +7,20 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-@WebServlet("MainServlet")
+
+import model.NewsService;
+
+@WebServlet("/MainServlet")
 public class MainServlet extends HttpServlet{
 @Override
 protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 	// TODO Auto-generated method stub
+//	super.doGet(req, resp);
+	NewsService newsService = (NewsService)req.getSession().getAttribute("newsService");
+	if(newsService==null) {
+		newsService = new NewsService();
+		req.getSession().setAttribute("newsService", newsService);
+	}
 	req.getRequestDispatcher("trangChu.jsp").forward(req, resp);
 }
 }
